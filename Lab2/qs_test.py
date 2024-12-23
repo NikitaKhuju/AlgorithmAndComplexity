c282e600dfc1e6a71bfbf26bfd777b58d365ecfc
Nikita <nikitashrestha345@gmail.com>
2024-12-23 21:40:16 +0545

    added testcases in lab2

--- Lab2/qs_test.py
+++ Lab2/qs_test.py
@@ -9,7 +9,10 @@ class TestQuickSort(unittest.TestCase):
             ([5,4,3,2,1], [1,2,3,4,5]), #reverse order
             ([1,1,1,1,1], [1,1,1,1,1]),#same elements
             ([1],[1]),#single element
-            ([],[])#no elements
+            ([],[]),#no elements
+            ([-3,-2,1,-5,4],[-5,-3,-2,1,4]),#negative elementss
+            ([3.1,2.2,5.5,4.3,1.7],[1.7,2.2,3.1,4.3,5.5]),#floating point numbers
+            ([2,5,3,3,5,2],[2,2,3,3,5,5])#duplicates
         ]
 
     def test_quick_sort(self):
